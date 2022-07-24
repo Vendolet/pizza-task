d1f705c526f33201aa229045f91cea6fb087de46
Vendolet <kolobov.m.s@yandex.ru>
2022-07-24 18:46:43 +0300

    feat: добавлены модули node (в т.ч. vuex) в проект; refactor: переименована папка pages в makets

---
+++ scripts/pizz-mock.js
@@ -0,0 +1,74 @@
+export const db = [
+    {
+        id: 1,
+        note: 'Пицца АМЕРИКАНО',
+        price: 500,
+        img: './access/img/americano.jpg'
+    },
+    {
+        id: 2,
+        note: 'Пицца БРОККОЛИ',
+        price: 520,
+        img: './access/img/broccole.jpg'
+    },
+    {
+        id: 3,
+        note: 'Пицца с цыплёнком',
+        price: 480,
+        img: './access/img/checkee.jpg'
+    },
+    {
+        id: 4,
+        note: 'Пицца классическая',
+        price: 450,
+        img: './access/img/classic.jpg'
+    },
+    {
+        id: 5,
+        note: 'Пицца СЫР',
+        price: 560,
+        img: './access/img/don-cheeze.jpeg'
+    },
+    {
+        id: 6,
+        note: 'Пицца МЯСНАЯ',
+        price: 620,
+        img: './access/img/meet.jpg'
+    },
+    {
+        id: 7,
+        note: 'Пицца ПАПРИКА',
+        price: 650,
+        img: './access/img/paprika.jpg'
+    },
+    {
+        id: 8,
+        note: 'Пицца ПЕППЕРОНИ',
+        price: 540,
+        img: './access/img/pepperoni-lajt.jpg'
+    },
+    {
+        id: 9,
+        note: 'Пицца ПРОВАНС',
+        price: 570,
+        img: './access/img/provance.jpg'
+    },
+    {
+        id: 10,
+        note: 'Пицца с беконом',
+        price: 540,
+        img: './access/img/pizza-bekon.jpg'
+    },
+    {
+        id: 11,
+        note: 'Пицца СОЛЯМИ',
+        price: 600,
+        img: './access/img/solyami.jpg'
+    },
+    {
+        id: 12,
+        note: 'Пицца томатная',
+        price: 480,
+        img: './access/img/tomato.jpg'
+    }
+];
