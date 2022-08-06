fb087a00c31bb77ac8d799aa0517c5b8e007a128
Vendolet <kolobov.m.s@yandex.ru>
2022-08-06 17:48:13 +0300

    fix: исправлен роут Basket

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\MainController;
+use App\Http\Controllers\BasketController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,4 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/', [MainController::class, 'index']);
-Route::post('/basket', [BasketController::class, 'index']);
+Route::post('/basket', [BasketController::class, 'store']);
