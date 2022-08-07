9ab2ef75a528088e075215fb07dece1935ea18f6
Vendolet <kolobov.m.s@yandex.ru>
2022-08-07 21:54:15 +0300

    feat: добавлена транзакция в basketController. Ограничена выборка столбцами в catalogController

--- backend/app/Http/Controllers/BasketController.php
+++ backend/app/Http/Controllers/BasketController.php
@@ -24,19 +24,28 @@ class BasketController extends Controller
         $order->name = $query_customer['name'];
         $order->phone = $query_customer['phone'];
         $order->address = $query_customer['address'];
-        $order->save();
 
-        $order_id = $order->id;
+        DB::beginTransaction();
+        try{
+            $order->save();
 
-        foreach ($query_basket as $item){
-            $product_id = $item['id'];
+            $order_id = $order->id;
 
-            DB::table('order_products')->insert([
-                'order_id' => $order_id,
-                'product_id' => $product_id
-            ]);
+            foreach ($query_basket as $item){
+                $product_id = $item['id'];
+
+                DB::table('order_products')
+                    ->insert([
+                        'order_id' => $order_id,
+                        'product_id' => $product_id
+                    ]);
+            }
+
+            DB::commit();
+            return response()->json(['code'=>200, 'message'=>"Successful"], 200);
+        }catch (\Exception $e){
+            DB::rollBack();
+            return response()->json(['code'=>500, 'message'=>$e], 500);
         }
-
-        return response()->json(['code'=>200, 'message'=>"Successful"], 200);
     }
 }
--- backend/app/Http/Controllers/MainController.php
+++ backend/app/Http/Controllers/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use Illuminate\Support\Facades\DB;
 
 class MainController extends Controller
 {
@@ -13,7 +14,11 @@ class MainController extends Controller
      */
     public function index()
     {
-        $products = Product::paginate(8);
+        // $products = Product::paginate(8);
+        // return response()->json($products, 200);
+        $products = DB::table('products')
+                        ->select('id', 'note', 'price', 'img', 'article')
+                        ->paginate(8);
         return response()->json($products, 200);
     }
 }
